47e2c7ea306c0c81974ba897f8de8036fa8a875a
sawal123 <sawalinto@gmail.com>
2026-09-05 13:31:00 +0700

    fix: standardize quotation marks in audio.js for consistency

--- public/tema/whitepre/src/js/audio.js
+++ public/tema/whitepre/src/js/audio.js
@@ -1,9 +1,9 @@
-const coverMobile = document.getElementById('cover-mobile');
-const openCoverButton = document.getElementById('open-cover');
+const coverMobile = document.getElementById("cover-mobile");
+const openCoverButton = document.getElementById("open-cover");
 
 if (openCoverButton) {
-  openCoverButton.addEventListener('click', function () {
-    coverMobile.classList.add('hidden');
-    window.musicPlayer?.play();
-  });
+    openCoverButton.addEventListener("click", function () {
+        coverMobile.classList.add("hidden");
+        window.musicPlayer?.play();
+    });
 }
